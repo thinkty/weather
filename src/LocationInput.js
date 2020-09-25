e65fd0c1bd70a48ba6300fe8302cae746d69497c
thinkty <thinkty@naver.com>
2020-09-26 00:24:42 +0900

    Added input component for location

---
+++ src/LocationInput.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+
+/**
+ * A component to take location (simple text) input
+ */
+export default class LocationInput extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: '',
+    };
+  }
+
+  onSubmit = (event) => {
+    event.preventDefault();
+    const { value } = this.state;
+    console.log(value);
+  }
+
+  onChange = (event) => {
+    this.setState({ value: event.target.value });
+  }
+
+  render() {
+    const { value } = this.state;
+
+    return (
+      <form onSubmit={this.onSubmit}>
+        <input
+          type="text"
+          autoFocus
+          placeholder="Enter location..."
+          value={value}
+          onChange={this.onChange}
+          style={{
+            width: '95%',
+            backgroundColor: 'transparent',
+            color: '#fff',
+            fontSize: '15px',
+            padding: 10,
+            borderStyle: 'none none solid none',
+            outline: 'none'
+          }}
+        />
+      </form>
+    );
+  }
+}
